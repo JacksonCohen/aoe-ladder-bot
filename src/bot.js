fcb4fa9a884c9f120a3c9547c6bfd4daebeb37d7
Jackson <jacksonrcohen@gmail.com>
2021-03-30 10:28:03 -0400

    Add EventEmitter checking for ! prefixed messages

--- src/bot.js
+++ src/bot.js
@@ -2,9 +2,21 @@ require('dotenv').config();
 
 const { Client } = require('discord.js');
 const client = new Client();
+const PREFIX = '!';
 
 client.on('ready', () => {
   console.log(`${client.user.username} has logged in.`);
+});
+
+client.on('message', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(PREFIX)) {
+    const [command, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+  }
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
