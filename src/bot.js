31527123a75d6dec7795cb12edb2e1d6086330dd
Jackson <jacksonrcohen@gmail.com>
2021-03-30 11:54:28 -0400

    Fetch ladder rankings from Sheets API

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,16 @@
+const axios = require('axios');
 require('dotenv').config();
 
 const { Client } = require('discord.js');
 const client = new Client();
+const { DISCORDJS_BOT_TOKEN, GOOGLE_API_KEY, SHEETS_ID } = process.env;
 const PREFIX = '!';
 
 client.on('ready', () => {
   console.log(`${client.user.username} has logged in.`);
 });
 
-client.on('message', (message) => {
+client.on('message', async (message) => {
   if (message.author.bot) return;
 
   if (message.content.startsWith(PREFIX)) {
@@ -16,13 +18,31 @@ client.on('message', (message) => {
       .trim()
       .substring(PREFIX.length)
       .split(/\s+/);
-  }
 
-  if (command === 'ladder') {
-  } else if (command === 'challenge') {
-  } else if (command === 'record') {
-  } else if (command === 'history') {
+    const users = message.guild.members.cache.map(({ user }) => user.username);
+
+    switch (command) {
+      case 'ladder': {
+        const { data } = await axios.get(
+          `https://sheets.googleapis.com/v4/spreadsheets/${SHEETS_ID}/values/Ranking!A2:B100?key=${GOOGLE_API_KEY}`,
+        );
+        const rankings = data.values.map((rank) => rank.join('. ')).join('\n');
+        return message.reply(rankings);
+      }
+      case 'challenge': {
+        if (args.length === 0) {
+          return message.reply('you gotta @ someone to challenge them');
+        }
+        break;
+      }
+      case 'record': {
+        break;
+      }
+      case 'history': {
+        break;
+      }
+    }
   }
 });
 
-client.login(process.env.DISCORDJS_BOT_TOKEN);
+client.login(DISCORDJS_BOT_TOKEN);
