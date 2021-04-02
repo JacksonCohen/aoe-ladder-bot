c6c637528aace928c7f4767b193f71a8964db72b
Jackson <jacksonrcohen@gmail.com>
2021-04-02 14:02:51 -0400

    Display rating alongside rankings

---
+++ profiles.js
@@ -0,0 +1,30 @@
+module.exports = {
+  Jax: {
+    username: 'Breadzilla',
+    id: 1981760,
+  },
+  MJ: {
+    username: 'mj_preston',
+    id: 3939970,
+  },
+  Lendle: {
+    username: 'glt1021',
+    id: 3985998,
+  },
+  Mattia: {
+    username: 'mattiapickett',
+    id: 5349124,
+  },
+  Lama: {
+    username: 'Lama1447',
+    id: 1943142,
+  },
+  Sam: {
+    username: 'GGets',
+    id: 3933802,
+  },
+  AK: {
+    username: 'akkaz',
+    id: 2117020,
+  },
+};
--- src/commands/ladder.js
+++ src/commands/ladder.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const profiles = require('../../profiles');
 const { GOOGLE_API_KEY, SHEETS_ID } = process.env;
 
 module.exports = {
@@ -6,10 +7,41 @@ module.exports = {
   description: 'Display the ladder for all to see.',
   guildOnly: true,
   async execute(message) {
-    const { data } = await axios.get(
+    const {
+      data: { values },
+    } = await axios.get(
       `https://sheets.googleapis.com/v4/spreadsheets/${SHEETS_ID}/values/Ranking!A2:B100?key=${GOOGLE_API_KEY}`,
     );
-    const rankings = data.values.map((rank) => rank.join('. ')).join('\n');
+
+    for (const player in profiles) {
+      const { data: soloStats } = await axios.get(
+        `https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=3&profile_id=${profiles[player].id}&count=1`,
+      );
+
+      const { data: teamStats } = await axios.get(
+        `https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=4&profile_id=${profiles[player].id}&count=1`,
+      );
+
+      for (const sheetsPlayer in values) {
+        if (values[sheetsPlayer][1] === player) {
+          soloStats[0] &&
+            values[sheetsPlayer].push(`1s - ${soloStats[0].rating}`);
+          teamStats[0] &&
+            values[sheetsPlayer].push(`Team - ${teamStats[0].rating}`);
+        }
+      }
+    }
+
+    const rankings = values
+      .map((rank) => {
+        const playerRanking = rank.slice(0, 2).join('. ');
+        const playerRating = rank.slice(2).join(' | ');
+        return playerRating
+          ? `${playerRanking} (${playerRating})`
+          : playerRanking;
+      })
+      .join('\n');
+
     return message.channel.send(rankings);
   },
 };
